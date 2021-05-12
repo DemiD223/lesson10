42ecb21db5311acf2122c3a40a85574c8de72da5
DemiD223 <ademenko.demid@gmail.com>
2021-05-12 19:29:43 +0300

    next and prev change

--- TVController.py
+++ TVController.py
@@ -30,17 +30,17 @@ class TVController:
         self.cur_ch -= 1
         if self.cur_ch < 0:
             return self.first_channel()
-        return self.turn_channel(self.cur_ch+1)
+        return self.turn_channel(self.cur_ch + 1)
 
     def current_channel(self):
         return self.chs[self.cur_ch]
 
-    def is_exist(self, num: int) -> str:
-        if num in range(len(self.chs)):
-            return "Yes"
-        return "No"
+    def is_exist(self, data) -> str:
+        if type(data) == int:
+            if data in range(len(self.chs) + 1):
+                return "Yes"
+            return "No"
 
-    def is_exist(self, name: str) -> str:
-        if name in self.chs:
+        if data in self.chs:
             return "Yes"
         return "No"
